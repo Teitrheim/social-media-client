48ad4633ebb331a182cd48e789386423ec6cab86
Thomas Eitrheim <teitrheim@gmail.com>
2024-01-22 23:24:31 +0100

    Added base URL to Cypress e2e test configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://127.0.0.1:5500/",
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       /* ... */
