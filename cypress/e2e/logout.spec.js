ee6264dc1ef6d9dd2040a5a4376905a99bfadc87
Thomas Eitrheim <teitrheim@gmail.com>
2024-01-25 19:15:06 +0100

    deleted

--- cypress/e2e/logout.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Logout functionality", () => {
-  it("allows the user to log out", () => {
-    cy.get('button[data-auth="logout"]').click();
-    cy.url().should("eq", "http://127.0.0.1:5500/index.html");
-  });
-});
